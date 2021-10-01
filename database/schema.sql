25ef8ea9140d3a366ac3862e60fffbae952cb58f
imjord <dean.jay.goat@gmail.com>
2021-10-01 17:51:38 -0600

    updated the schema to match the design required in challenge

--- database/schema.sql
+++ database/schema.sql
@@ -5,6 +5,19 @@ USE Employee_Tracker; -- USING MY DATABSE
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,  -- id that auto increments whenever a new id is assigned primary key unique identifyer 
-    first_name VARCHAR(60) NOT NULL, -- string max 60 length not null cant accept null values 
-    last_name VARCHAR(60) NOT NULL, 
+    first_name VARCHAR(30) NOT NULL, -- string max 30 length not null cant accept null values 
+    last_name VARCHAR(30) NOT NULL, 
+    rold_id INT
+    manager_id INT
+)
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL
+    salary DECIMAL -- an exact fixed-point number according to w3schools lol
+)
+
+CREATE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
 )
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 const inquirer = require('inquirer'); // import in quirer
+const cTable = require('console.table') // import required package for assignemnt never used before 
+
+
+
