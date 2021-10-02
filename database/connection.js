9a759336eed8f05c1d1cc09df08d3ff4216e681f
imjord <dean.jay.goat@gmail.com>
2021-10-02 15:02:36 -0600

    fixed error regarding tables not showing up with query with help from my abcbc bro

--- database/connection.js
+++ database/connection.js
@@ -2,16 +2,21 @@ const mysql = require('mysql2'); // CONNECT TO MYSQL
 
 
 // Connect to database
-const db = mysql.createConnection(
+const db =  mysql.createConnection(
     {
       host: 'localhost',
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
-      password: '1290BecomeEzdeveloper123',
+      password: '1290BecomeEZdeveloper123',
       database: 'employee_tracker'
     },
     console.log('Connected to the EMPLOYEE database.')
   );
+
+
+  db.connect(function (err) {  
+    if (err) throw err;
+    });
 
   module.exports = db;  // export me db 
--- index.js
+++ index.js
@@ -1,20 +1,113 @@
 const inquirer = require('inquirer'); // import in quirer
-const cTable = require('console.table') // import required package 
+require('console.table') // import required package 
 const db = require('./database/connection.js'); // import my database 
 
-db.query('SELECT * FROM department', (err, rows) => {
-    console.log(rows)
-})
 
-// prompt the user with questions NODE INDEX
-// inquirer.prompt([
-//     {
-//         type:'list',
-//         name: 'myChoices',
-//         message: 'What would you like to do?',
-//         choices: ["View all employees", "View all employess by department", "View employess by manager", "add employee", "remove employee", "updated employee role", "update employee manager"],
-//         default: 'View all employees'
-//     }
-// ]).then(answers => {
-// console.log('Answer:' + answers) // console log what user checkboxes
-// });
+
+    // can sucessfully query from table
+    db.query('SELECT * FROM employee;', (err, rows) => {
+        console.log(err);    
+    console.table(rows)})
+
+
+    prompt the user with questions NODE INDEX
+    inquirer.prompt([
+        {
+            type:'list',
+            name: 'myChoices',
+            message: 'What would you like to do?',
+            choices: ["View all employees", "View all employess by department", "View employess by manager", "add employee", "remove employee", "updated employee role", "update employee manager"],
+            default: 'View all employees'
+        }
+    ]).then(answers => {
+    console.log('Answer:' + answers) // console log what user checkboxes
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ignore these just debuggins with abcbc assistant
+    // db.query('SELECT * FROM employee', (err, rows) => {    
+    //     let departments = rows
+    //     console.table(departments)
+    //     })
+
+
+
+// function randomfact() {
+//     let sql = 'SELECT * FROM employee;';
+//     return new Promise((resolve, reject) => {
+//       db.query(sql, (err, result) => {
+//         if (err) {
+//           reject(err);
+//         }
+//         else {
+//           resolve(result);
+//         }
+//       });
+//     });
+//   }
+
+
+//   function runQuery() {
+//     const result = await randomfact();
+    
+//     console.log(randomfact());
+//  };
+
+
+//  runQuery();
+
