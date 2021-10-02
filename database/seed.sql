7be5bc64e47dd4d31f0fd361ea607b1256f72b97
imjord <dean.jay.goat@gmail.com>
2021-10-01 18:27:14 -0600

    Added seed data my fingaers

---
+++ database/seed.sql
@@ -0,0 +1,24 @@
+INSERT INTO employee (first_name, last_name, rold_id, manager_id) VALUES 
+    ('COCO', 'BEAN', 1, 1),
+    ('Jordan', 'Bro', 2, 2),
+    ('GeGe', 'Bark', 3, NULL),
+    ('Mad', 'Hatter', 4, NULL),
+    ('Keanu', 'Reaves', 6, 10),
+    ('Brad', 'Pitt', 42, 12),
+    ('Tj', 'Roberts', 1, NULL);
+--  USING SEMI COLON TO END QUERY
+INSERT INTO roleboi(title, salary, department_id) VALUES
+('Ruler of the world', 1.000.000.000, 1),
+('Ruler of the house', 100.000, 2),
+('Boss person', 10.000, 3),
+('Janitor', 50.000, 4),
+('Model', 600.000, 6),
+('Actor', 700.000, 42),
+('Cool guy', 700.700.700, 1);
+
+INSERT INTO department (name) VALUES
+('OVERLORDS'),
+('COOL PEOPLE'),
+('BOSSES'),
+('GAMER');
+
