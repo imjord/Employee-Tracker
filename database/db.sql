34e1dff53f76ce1c4418031fa62ebcee47b97ac1
imjord <dean.jay.goat@gmail.com>
2021-10-01 18:35:56 -0600

    getting mysql error in shell

--- database/db.sql
+++ database/db.sql
@@ -1,3 +1,5 @@
 DROP DATABASE IF EXISTS Employee_Tracker;  -- DROPS THE DATABASE IF EMPLOYEE TRACKER IS ALREADY 
 CREATE DATABASE Employee_Tracker; -- MY DATABASE 
 USE Employee_Tracker; -- USING MY DATABSE 
+
+-- mysql -u root -p
