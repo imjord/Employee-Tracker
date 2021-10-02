7836c2ab56058e5172e2671fdfc08afd58d890ac
imjord <dean.jay.goat@gmail.com>
2021-10-01 18:13:12 -0600

    added db file to put db in and added drop tables in schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,8 @@
-DROP DATABASE IF EXISTS Employee_Tracker;  -- DROPS THE DATABASE IF EMPLOYEE TRACKER IS ALREADY 
-CREATE DATABASE Employee_Tracker; -- MY DATABASE 
-USE Employee_Tracker; -- USING MY DATABSE 
 
+
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS roleboi; -- made it roleboi because role was blue and was triggering me
+DROP TABLE IF EXISTS department;
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,  -- id that auto increments whenever a new id is assigned primary key unique identifyer 
@@ -9,10 +10,10 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL, 
     rold_id INT,
     manager_id INT,
-    CONSTRAINT fk_roleboi FOREIGN KEY (role_id) REFERENCES role(id)
+    CONSTRAINT fk_roleboi FOREIGN KEY (roleboi_id) REFERENCES roleboi(id)
 )
 
-CREATE TABLE role (
+CREATE TABLE roleboi (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL, -- an exact fixed-point number according to w3schools lol
