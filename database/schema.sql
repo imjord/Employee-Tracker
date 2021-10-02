5e4ea3da55121430f9c30988dc48aeaee17c5d15
imjord <dean.jay.goat@gmail.com>
2021-10-02 13:41:33 -0600

    was getting errors when trying to source my schema to mysql made my tables now

--- database/schema.sql
+++ database/schema.sql
@@ -4,24 +4,36 @@ DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS roleboi; -- made it roleboi because role was blue and was triggering me
 DROP TABLE IF EXISTS department;
 
-CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,  -- id that auto increments whenever a new id is assigned primary key unique identifyer 
-    first_name VARCHAR(30) NOT NULL, -- string max 30 length not null cant accept null values 
-    last_name VARCHAR(30) NOT NULL, 
-    rold_id INT,
-    manager_id INT,
-    CONSTRAINT fk_roleboi FOREIGN KEY (roleboi_id) REFERENCES roleboi(id)
-)
 
-CREATE TABLE roleboi (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+-- CREATE department (
+--     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(30)
+-- );
+
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL, -- an exact fixed-point number according to w3schools lol
-    department_id INT,
-    CONSTRAINT fk_departmentboi FOREIGN KEY (department_id) REFERENCES department(id)   -- USING FOREIGN KEY TO REFERENCE THE PK IN OTHER TABLE 
-)
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)   -- USING FOREIGN KEY TO REFERENCE THE PK IN OTHER TABLE 
+);
 
-CREATE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
-)
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,  -- id that auto increments whenever a new id is assigned primary key unique identifyer 
+    first_name VARCHAR(30) NOT NULL, -- string max 30 length not null cant accept null values 
+    last_name VARCHAR(30) NOT NULL, 
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id)
+);
+
+
+
--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const inquirer = require('inquirer'); // import in quirer
-const cTable = require('console.table') // import required package for assignemnt never used before 
+const cTable = require('console.table') // import required package 
 const db = require('./database/connection'); // import my database 
 
-
+db.query('SELECT * FROM roleboi', (err, rows) => {
+    console.log(rows)
+})
 
 // prompt the user with questions NODE INDEX
-inquirer.prompt([
-    {
-        type:'checkbox',
-        name: 'myChoices',
-        message: 'What would you like to do?',
-        choices: ["View all employees", "View all employess by department", "View employess by manager", "add employee", "remove employee", "updated employee role", "update employee manager"],
-        default: 'View all employees'
-    }
-]).then(answers => {
-console.log('Answer:' + answers) // console log what user checkboxes
-});
+// inquirer.prompt([
+//     {
+//         type:'list',
+//         name: 'myChoices',
+//         message: 'What would you like to do?',
+//         choices: ["View all employees", "View all employess by department", "View employess by manager", "add employee", "remove employee", "updated employee role", "update employee manager"],
+//         default: 'View all employees'
+//     }
+// ]).then(answers => {
+// console.log('Answer:' + answers) // console log what user checkboxes
+// });
