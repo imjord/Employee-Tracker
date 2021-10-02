e6a2cbf68f245f79ac81256ca64e37fdd8884287
imjord <dean.jay.goat@gmail.com>
2021-10-02 11:50:54 -0600

    have a working inquireri prompt

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const cTable = require('console.table') // import required package for assignemn
 
 
 
+inquirer.prompt([
+    {
+        type:'checkbox',
+        name: 'myChoices',
+        message: 'What would you like to do?',
+        choices: ["View all employees", "View all employess by department", "View employess by manager", "add employee", "remove employee", "updated employee role", "update employee manager"],
+        default: 'View all employees'
+    }
+]).then(answers => {
+console.log('Answer:' + answers)
+});
